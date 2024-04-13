88610c55d5407ee20cf37bfff56ac5ef8fdf27c8
“JEREMY <Jeremyinoa67@gmail.com>
2024-04-12 21:11:44 -0400

    feat: adds h2 atom for variance

---
+++ src/atoms/H2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const H1 = ({h1Text}) => {
+    return (
+        <h1 className='text-center py-3 bg-gray-900  w-3/4 mx-auto text-1xl font-extrabold leading-none text-white md:text-2xl lg:text-3xl'>
+            {h1Text}
+        </h1>   
+    )
+}
+
+export default H1
